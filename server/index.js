5967fb244c0c37670ccbf01b52f53c7b7be8aae1
Deepan59 <revathideepan59@gmail.com>
2026-08-13 20:02:16 +0530

    fixing cors policy

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ const upload = multer({
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173','https://ai-tailor-resume-5eqs.onrender.com'],
     credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
